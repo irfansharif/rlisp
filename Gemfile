61fb6c8ccd45e1b6a98a3ad62b9235f32ce03c87
Irfan Sharif <irfan.sharif@shopify.com>
2016-04-08 01:31:42 -0400

    tokenizer, AST generator implemented

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in rlisp.gemspec
 gemspec
+gem 'pry-nav', group: [:development, :test]
--- lib/rlisp.rb
+++ lib/rlisp.rb
@@ -1,5 +1,39 @@
 require "rlisp/version"
 
-module Rlisp
-  # Your code goes here...
+module Lisp
+  extend self
+
+  def tokenize(program)
+    tokens = program
+      .gsub('(', ' ( ')
+      .gsub(')', ' ) ').strip.split(/ /)
+    tokens.delete ""
+
+    tokens
+  end
+
+  def generate_abstract_syntax_tree(tokens)
+    raise if tokens.length == 0 or tokens.first == ')'
+    if tokens.first == '('
+      tokens.shift
+      abstract_syntax_tree = []
+      while tokens.first != ')' do
+        sub_tree = generate_abstract_syntax_tree(tokens)
+        abstract_syntax_tree << sub_tree
+      end
+
+      tokens.shift
+      return abstract_syntax_tree
+    else
+      return ASTNode.new tokens.shift
+    end
+  end
+end
+
+class ASTNode
+  attr_accessor :type
+
+  def initialize(param_type)
+    self.type = param_type
+  end
 end
